203791e7f916703ef0283b85f2d1a711830cd1e6
jpromanonet <programming@jpromano.net>
2020-10-04 18:47:16 -0300

    Raw mode

--- texty.c
+++ texty.c
@@ -2,6 +2,14 @@
 #include <termios.h>
 #include <unistd.h>
 
+void enableRawMode()
+{
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main()
 {
     char c;
