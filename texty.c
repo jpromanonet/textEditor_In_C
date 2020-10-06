27a9e4387021feaf52614fa31a1bbc421170c2e9
jpromanonet <programming@jpromano.net>
2020-10-06 20:42:48 -0300

    Raw mode

--- texty.c
+++ texty.c
@@ -3,13 +3,20 @@
 #include <termios.h>
 #include <unistd.h>
 
+// This structure allows the program
+// to hide the shell and start the console
+// as a blank screen
 struct termios orig_termios;
 
+// This function disables de Raw mode when we
+// Press Q to exit the program
 void disableRawMode()
 {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
+// This one activates the Raw mode
+// when we start the program
 void enableRawMode()
 {
     tcgetattr(STDIN_FILENO, &orig_termios);
