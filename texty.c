4a959627d070878c3588e1279e0055eeee9e69ec
jpromanonet <programming@jpromano.net>
2020-10-11 20:46:48 -0300

    Fix Ctrl M

--- texty.c
+++ texty.c
@@ -24,7 +24,7 @@ void enableRawMode()
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
     struct termios raw = orig_termios;
-    raw.c_iflag &= ~(IXON);
+    raw.c_iflag &= ~(ICRNL | IXON);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
