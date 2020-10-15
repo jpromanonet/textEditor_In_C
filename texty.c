0666a9bad5913977a40f628b15903c134cc4bbe1
jpromanonet <programming@jpromano.net>
2020-10-15 16:12:00 -0300

    Turn off flags

--- texty.c
+++ texty.c
@@ -1,4 +1,11 @@
-// First, i call the libraries i need
+// Let's define de environment
+#define TEXTY_VERSION "0.0.1"
+
+#ifdef __linux__
+#define _POSIX_C_SOURCE 200809L
+#endif
+
+// Then call the libraries we need
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,16 +26,39 @@ void disableRawMode()
 
 // This one activates the Raw mode
 // when we start the program
-void enableRawMode()
+int enableRawMode(int fd)
 {
-    tcgetattr(STDIN_FILENO, &orig_termios);
-    atexit(disableRawMode);
-    struct termios raw = orig_termios;
+    struct termios raw;
+
+    if (E.rawmode)
+        return 0; /* Already enabled. */
+    if (!isatty(STDIN_FILENO))
+        goto fatal;
+    atexit(editorAtExit);
+    if (tcgetattr(fd, &orig_termios) == -1)
+        goto fatal;
+
+    raw = orig_termios; /* modify the original mode */
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    /* output modes - disable post processing */
     raw.c_oflag &= ~(OPOST);
+    /* control modes - set 8 bit chars */
     raw.c_cflag |= (CS8);
+    /* local modes - choing off, canonical off, no extended functions,
+     * no signal chars (^Z,^C) */
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_cc[VMIN] = 0;  /* Return each byte, or zero for timeout. */
+    raw.c_cc[VTIME] = 1; /* 100 ms timeout (unit is tens of second). */
+
+    /* put terminal in raw mode after flushing */
+    if (tcsetattr(fd, TCSAFLUSH, &raw) < 0)
+        goto fatal;
+    E.rawmode = 1;
+    return 0;
+
+fatal:
+    errno = ENOTTY;
+    return -1;
 }
 
 int main()
