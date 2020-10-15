54eb1d04805718a389083a8ed79daf558f7c0feb
jpromanonet <programming@jpromano.net>
2020-10-15 15:59:26 -0300

    Disable raw

--- texty.c
+++ texty.c
@@ -25,7 +25,7 @@ void enableRawMode()
     atexit(disableRawMode);
     struct termios raw = orig_termios;
     raw.c_iflag &= ~(ICRNL | IXON);
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    raw.c_oflag &= ~(OPOST);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
